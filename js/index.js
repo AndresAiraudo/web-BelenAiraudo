2e3099ef41c6cf9ea415d45d9be4d02a6b2cdd20
Andres Airaudo <andres.airaudo97@gmail.com>
2022-02-20 01:11:42 -0300

    Termino formulario 1er version

--- js/index.js
+++ js/index.js
@@ -1,3 +1,36 @@
-$(document).ready(function(){
-    $('.carousel').carousel({interval: 7000});
+const inputs = document.querySelectorAll('.campo input');
+inputs.forEach( input =>{
+    input.onfocus = ()=>{
+        input.previousElementSibling.classList.add('top');
+        input.previousElementSibling.classList.add('focus');
+        input.parentNode.classList.add('focus');
+    }
+
+    input.onblur = ()=>{
+        input.value = input.value.trim( );
+        if(input.value.trim( ).length == 0){
+            input.previousElementSibling.classList.remove('top');
+        }
+        input.previousElementSibling.classList.remove('focus');
+        input.parentNode.classList.remove('focus');
+    }
 });
+
+const textar = document.querySelectorAll('.campo textarea');
+textar.forEach( textarea =>{
+    textarea.onfocus = ()=>{
+        textarea.previousElementSibling.classList.add('top');
+        textarea.previousElementSibling.classList.add('focus');
+        textarea.parentNode.classList.add('focus');
+    }
+
+    textarea.onblur = ()=>{
+        textarea.value = textarea.value.trim( );
+        if(textarea.value.trim( ).length == 0){
+            textarea.previousElementSibling.classList.remove('top');
+        }
+        textarea.previousElementSibling.classList.remove('focus');
+        textarea.parentNode.classList.remove('focus');
+    }
+});
+
